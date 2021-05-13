e9250e40c60435056a07d432a65a76c79793bb97
Vitor-rpaco <43481335+Vitor-rpaco@users.noreply.github.com>
2021-05-13 13:00:25 -0300

    Funcionalidade de listagem adicionada

--- listarTodos.php
+++ listarTodos.php
@@ -1,35 +1,31 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $operacao = $_POST["Op"];
-    echo "Operação Selecionada: $operacao";
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$nomeBanco = "lojaphp";
 
-    $servidor = "localhost";
-    $usuario = "root";
-    $senha = "";
-    $nomeBanco = "dawfaeterj";
+//Estabelecer conexão com o servidor
+$conn = new mysqli($servidor, $usuario, $senha, $nomeBanco);
+if ($conn->connect_error) {
+    die("Conexao falhou: " . $conn->connect_error);
+}
 
-    //Estabelecer conexão com o servidor
-    $conn = new mysqli($servidor, $usuario, $senha, $nomeBanco);
-    if ($conn->connect_error) {
-        die("Conexao falhou: " . $conn->connect_error);
-    }
+//Lista cliente
+$sql = "SELECT * FROM clientes";
+$result = $conn->query($sql);
 
-    //Lista aluno
-    if ($listarAluno == 'listarAluno') {
-        $sql = "SELECT * FROM `alunos`";
-        $result = $conn->query($sql);
-        echo $sql;
-        echo "<br><br>";
-        if ($result->num_rows > 0) {
-            while ($linha = $result->fetch_assoc()) {
-                echo "id: " . $linha["id"]
-                    . " Nome: " . $linha["nome"]
-                    . " Email: " . $linha["email"]
-                    . " Email: " . $linha["matricula"];
-               echo "<br><br>";
-            }
-        }
+if ($result->num_rows > 0) {
+    while ($linha = $result->fetch_assoc()) {
+        echo "id: " . $linha["idCliente"]
+            . " Nome: " . $linha["nome"]
+            . " Endereco: " . $linha["endereco"]
+            . " cep: " . $linha["cep"]
+            . " Cidade: " . $linha["cidade"]
+            . " Estado: " . $linha["estado"]
+            . " cpf: " . $linha["cpf"];
+       echo "<br><br>";
     }
 }
+
 ?>
